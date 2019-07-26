304dbb8ef507f57a6a536835d4e9816cf4367aac
Nyirate <rwakaemma34@gmail.com>
2019-07-26 12:37:18 +0200

    add jquery functions of hiding and showing

---
+++ index.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+ $(".img1").click(function(){
+ $(".des").show();
+ $(".img1").hide();
+});
+$(".des").click(function(){
+  $(".img1").show();
+  $(".des").hide();
+ });
+ $(".img2").click(function(){
+  $(".dev").show();
+  $(".img2").hide();
+ });
+ $(".dev").click(function(){
+  $(".img2").show();
+  $(".dev").hide();
+ });
+ $(".img3").click(function(){
+  $(".prod").show();
+  $(".img3").hide();
+ });
+ $(".prod").click(function(){
+  $(".img3").show();
+  $(".prod").hide();
+ });
+});
