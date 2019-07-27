0f3abf9f37c72733b24aa7dbd809c6021c68bea3
Nyirate <rwakaemma34@gmail.com>
2019-07-27 20:02:41 +0200

    add media query

--- index.js
+++ index.js
@@ -1,26 +1,40 @@
 $(document).ready(function(){
- $(".img1").click(function(){
+ $(".img9").click(function(){
  $(".des").show();
- $(".img1").hide();
+ $(".img9").hide();
 });
 $(".des").click(function(){
-  $(".img1").show();
+  $(".img9").show();
   $(".des").hide();
  });
- $(".img2").click(function(){
+ $(".img10").click(function(){
   $(".dev").show();
-  $(".img2").hide();
+  $(".img10").hide();
  });
  $(".dev").click(function(){
-  $(".img2").show();
+  $(".img10").show();
   $(".dev").hide();
  });
- $(".img3").click(function(){
+ $(".img11").click(function(){
   $(".prod").show();
-  $(".img3").hide();
+  $(".img11").hide();
  });
  $(".prod").click(function(){
-  $(".img3").show();
+  $(".img11").show();
   $(".prod").hide();
  });
+
+$("#form").click(function(){
+  var name = $("#name").val()
+  var email = $("#mail").val()
+  var message = $("#msg").val();
+  if((name !== "") && (email !=="") && (message !=="")){
+    alert(name + " " + "thank you for working with us!")
+  }
+  else {
+     alert("The form is empty");
+  }
+  event.preventDefault();
 });
+});
+
